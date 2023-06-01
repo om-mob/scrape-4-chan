bb97ed731870014e98d88e71124baf19ced22cc4
name <omar.h_mobarak@yahoo.com>
2023-06-01 15:34:44 +0300

    ⚡⚡ modified the request connection maximum timeout to make it more suitable with big size files and slow internet connection

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -2,3 +2,5 @@ import path from "path"
 
 export const DATA_PATH = path.join(__dirname, "..", ".." , "data")
 export const ARCHIVE_PATH = path.join(DATA_PATH, "archive.txt")
+
+export const MAX_REQUEST_TIMEOUT = 100000
--- src/send_requests/send_requests.ts
+++ src/send_requests/send_requests.ts
@@ -11,6 +11,7 @@ import superagent, { Response, ResponseError } from "superagent";
 import { catch_errors } from "../utils/catch_errors";
 
 // import configs
+import { MAX_REQUEST_TIMEOUT } from "../config/consts"
 
 /**** Module ****/
 
@@ -18,7 +19,7 @@ type GetRequest = (url: string) => Promise<[Response, ResponseError]>;
 type GetRequestRaw = (url: string) => Promise<Response>;
 
 export const get_request: GetRequest = catch_errors<GetRequestRaw, GetRequest>(
-  async (url) => await superagent.get(url).timeout(60000)
+  async (url) => await superagent.get(url).timeout(MAX_REQUEST_TIMEOUT)
 );
 
 type PostRequest = <TPayload>(url: string, payload: TPayload) => Promise<[Response, ResponseError]>;
